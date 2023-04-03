122bf26350684d21700854ca474e61dd643e2701
DaffaPutra7 <putradika3604@gmail.com>
2023-04-03 11:14:33 +0700

    int main(1)

--- ActivityStruct3/ActivityStruct3.cpp
+++ ActivityStruct3/ActivityStruct3.cpp
@@ -22,6 +22,15 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cout << "Masukkan NIM : ";
 		cin.getline(mhs[i].NIM, 12);
-
+		cout << "Masukkan Nama : ";
+		cin.getline(mhs[i].nama, 25);
+		cout << "Alamat : ";
+		cout << "\nMasukkan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\nMasukkan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukkan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
 	}
 }
