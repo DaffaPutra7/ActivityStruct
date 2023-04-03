c8e4c49d2cda1f601d3aaa755c8d98d9345a60dd
DaffaPutra7 <putradika3604@gmail.com>
2023-04-03 10:23:39 +0700

    int main (1)

--- ActivityStruct/ActivityStruct.cpp
+++ ActivityStruct/ActivityStruct.cpp
@@ -11,7 +11,21 @@ struct mahasiswa
 
 
 int main() {
-	mahasiswa mmhs1;
+	mahasiswa mhs1, mhs2;
 
+	mhs1.NIM = "20220140133";
+	mhs1.nama = "Daffa Putradika Pratama";
+	mhs1.alamat = "Sangatta";
+	mhs1.umur = 18;
 
+	cout << "Masukan NIM : ";
+	cin >> mhs2.NIM;
+	cout << "Masukkan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan Umur : ";
+	cin >> mhs2.umur;
+
+	cout << 
 }
